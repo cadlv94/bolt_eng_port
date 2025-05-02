2565ae695fd2e4fc5b69e5e5a997ec7d8fa0f990
cadlv94 <carlosadlv18@gmail.com>
2025-05-01 23:58:17 -0400

    Update site content

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://cadlv94.github.io/bolt_eng_port/',
+  base: '/bolt_eng_port/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
